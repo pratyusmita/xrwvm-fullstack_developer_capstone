de9ecf83cae936e4f5bdab9faaaebf1ba4e29e5d
pratyusmita <pratyusmita@gmail.com>
2025-05-16 12:26:45 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
+ from django.db import models
+ from django.utils.timezone import now
 # from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -12,7 +12,12 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
