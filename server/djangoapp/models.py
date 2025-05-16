6b75e1c6bef2f19b5b5e9305ce1c8d9850baaca4
pratyusmita <pratyusmita@gmail.com>
2025-05-16 12:27:19 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,7 +2,7 @@
 
  from django.db import models
  from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
